e155dbdf68b886a16b8cc5ac675ef2e79fd4d74f
npaudyal321 <naween321@gmail.com>
2023-04-04 21:30:37 +0545

    update and delete in crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, create
+from .views import home, create, update, delete
 
 
 urlpatterns = [
     path("", home, name='home'),
-    path("create/", create, name="create")
+    path("create/", create, name="create"),
+    path("update/<int:id>/", update, name="update"),
+    path("delete/<int:id>/", delete, name="delete"),
 ]
--- crud/views.py
+++ crud/views.py
@@ -18,3 +18,37 @@ def create(request):
         return redirect("home")
     context = {"title": "Add Person"}
     return render(request, template_name="crud/add_person.html", context=context)
+
+
+def update(request, id):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        department = request.POST.get("department")
+        status = True if request.POST.get("status") else False
+        Person.objects.filter(id=id).update(name=name,
+                                            email=email,
+                                            age=age,
+                                            department=department,
+                                            is_active=status)
+        return redirect("home")
+    try:
+        person = Person.objects.get(id=id)
+    except Person.DoesNotExist:
+        return redirect('home')
+    checked = "checked" if person.is_active else ""
+    context = {"title": "Update Person", "person": person, "checked": checked}
+    return render(request, "crud/update_person.html", context=context)
+
+
+def delete(request, id):
+    if request.method == "POST":
+        Person.objects.filter(id=id).delete()
+        return redirect("home")
+    try:
+        person = Person.objects.get(id=id)
+    except Person.DoesNotExist:
+        return redirect("home")
+    context = {"title": "Delete Person", "person": person}
+    return render(request, "crud/delete_person.html", context=context)
